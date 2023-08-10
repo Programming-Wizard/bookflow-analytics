01a96fd9de884d2cfb2d6cf3d6baa9de1cb732d8
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 21:07:37 +0530

    added getters for the private variables

--- bookFlowAnalytics/src/application/Book.java
+++ bookFlowAnalytics/src/application/Book.java
@@ -1,6 +1,19 @@
 package application;
 
 public class Book {
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getCoverUrl() {
+		return coverUrl;
+	}
+
+
 	private String title;
 	private String author;
 	private String coverUrl;
