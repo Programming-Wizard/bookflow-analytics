8297b1d4a663adb9f48c5779881a737354620dac
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 22:06:16 +0530

    added the listener the scene to prevent when the user exits full screen the layout doesnt get disoriented

--- bookFlowAnalytics/src/application/Main.java
+++ bookFlowAnalytics/src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 
@@ -14,11 +16,20 @@ public class Main extends Application {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainWindow.fxml"));
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
+			mainWindowController MainWindow = loader.getController();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("BookFlow");
 			primaryStage.setFullScreen(true);
 			primaryStage.setScene(scene);
+			
+			scene.setOnKeyPressed(Event ->{
+				if(Event.getCode() == KeyCode.ESCAPE)
+				{
+					MainWindow.handleResize();
+				}
+			});
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
