fe09142979bb1cc3e43b9dd8436fd0d51c756ad8
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 19:50:14 +0530

    added the book class which is going to be used in the api class

---
+++ bookFlowAnalytics/src/application/Book.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Book {
+	private String title;
+	private String author;
+	private String coverUrl;
+	
+	public Book(String title, String author, String coverUrl)
+	{
+		this.title = title;
+		this.author = author;
+		this.coverUrl = coverUrl;
+	}
+	
+}
