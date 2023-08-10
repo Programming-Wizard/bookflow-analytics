e5ea1e124d7446bcf99db9dd81d874ebaae1f930
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 18:10:34 +0530

    defining controller class for its corresponding scene

---
+++ bookFlowAnalytics/src/application/mainWindowController.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+public class mainWindowController implements Initializable{
+	
+	@FXML
+	private HBox centerBox;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+
+}
