e2e9fd21a17d1fb12f8aec2678e30a1f8bf5f6fb
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 21:08:14 +0530

    made the primarystage go full screen

--- bookFlowAnalytics/src/application/Main.java
+++ bookFlowAnalytics/src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("BookFlow");
+			primaryStage.setFullScreen(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
