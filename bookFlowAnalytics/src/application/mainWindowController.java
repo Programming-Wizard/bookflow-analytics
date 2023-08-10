b7a17fd386d4b34299b8de122f1ef8d40e99a27d
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-10 21:12:57 +0530

    removed the Hbox and replaced it with a grid for more better control over displaying of books also made the boks pack in a stackpane before adding them to the grid for future editing purposes, encountered a bug where the books stacked upon each other, turns out the logic i wrote for the packing of cover pages in stackpane was afterwards and therefore every book got displayed in single cell of the grid also changed the output the api from maxresults of 10(which is its default) to 20 to complete the grid.

--- bookFlowAnalytics/src/application/mainWindowController.java
+++ bookFlowAnalytics/src/application/mainWindowController.java
@@ -1,21 +1,71 @@
 package application;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
-public class mainWindowController implements Initializable{
-	
+public class mainWindowController implements Initializable {
+
 	@FXML
-	private HBox centerBox;
+	private GridPane centerBox;
+
+	private GoogleBooksApiClient apiClient;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		
+		apiClient = new GoogleBooksApiClient();
+
+		String query = "fiction";
+		int startIndex = 0;
+		int maxResultsPerPage = 20;
+
+		List<Book> bookdata = apiClient.fetchBooksData(query);
+
+		int row = 0;
+		int col = 0;
+
+		for (Book book : bookdata) {
+			StackPane stackPane = new StackPane();
+			ImageView coverPage = new ImageView(new Image(book.getCoverUrl()));
+			coverPage.setFitHeight(150);
+			coverPage.setPreserveRatio(true);
+
+			coverPage.setOnMouseClicked(event -> {
+				System.out.println("clicked on book : " + book.getTitle());
+			});
+
+			Label titleLabel = new Label(book.getTitle());
+			titleLabel.setWrapText(true);
+			titleLabel.setMaxWidth(150); // Set a maximum width for the label
+
+			stackPane.getChildren().addAll(coverPage);
+
+			GridPane.setRowIndex(stackPane, row);
+			GridPane.setColumnIndex(stackPane, col);
+
+			centerBox.getChildren().add(stackPane);
+
+			col++;
+			if (col > 3) { // Display 3 books per row
+				col = 0;
+				row++;
+				
+				if(row > 4)
+				{
+					break;
+				}
+			}
+
+		}
+
 	}
 
 }
