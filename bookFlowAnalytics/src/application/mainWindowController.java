5cf290fe9b5fd102227156254bb5edb28ac5048e
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-11 20:53:25 +0530

    removed all that code and just assigned stackpane a css class and made the stackpanes move using css approach, this way i can not only avoid all that big lines of code but also it also feels modular way

--- bookFlowAnalytics/src/application/mainWindowController.java
+++ bookFlowAnalytics/src/application/mainWindowController.java
@@ -29,11 +29,6 @@ public class mainWindowController implements Initializable {
 
 	private int startIndex = 0;
 	private int maxResultsPerPage = 20;
-	private List<Timeline> hoverAnimations  = new ArrayList<>();
-	private List<Timeline> notHoveredAnimations = new ArrayList<>();
-	private ImageView hoveredImageView;
-	public ImageView coverPage;
-
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -51,14 +46,11 @@ public class mainWindowController implements Initializable {
 			for (Book book : bookdata) 
 			{
 				StackPane stackPane = new StackPane();
-				coverPage = new ImageView(new Image(book.getCoverUrl()));
+				ImageView coverPage = new ImageView(new Image(book.getCoverUrl()));
 				coverPage.setFitHeight(150);
 				coverPage.setPreserveRatio(true);
-				createHoverAnimation(coverPage);
-				createNotHoverAnimation(coverPage);
 
-				coverPage.setOnMouseEntered(event -> MouseEntered(coverPage));
-				coverPage.setOnMouseExited(event -> MouseLeft(coverPage));
+				stackPane.getStyleClass().add("cover-image");
 
 				Label titleLabel = new Label(book.getTitle());
 				titleLabel.setWrapText(true);
@@ -88,54 +80,9 @@ public class mainWindowController implements Initializable {
 
 	}
 
-
-	private void createHoverAnimation(ImageView coverPage)
-	{
-		Timeline hoverAnimation = new Timeline();
-		KeyFrame keyframe1 = new KeyFrame(javafx.util.Duration.ZERO, new KeyValue(coverPage.translateYProperty(),0));
-		KeyFrame keyframe2 = new KeyFrame(javafx.util.Duration.millis(50), new KeyValue(coverPage.translateYProperty(),-5));
-		hoverAnimation.getKeyFrames().addAll(keyframe1,keyframe2);
-	}
-	public void MouseEntered(ImageView coverPage)
-	{
-	       int index = centerBox.getChildren().indexOf(coverPage.getParent());
-	        if (index >= 0 && index < hoverAnimations.size()) {
-	            hoverAnimations.get(index).play();
-	        }
-		}
-
-	private void createNotHoverAnimation(ImageView coverPage)
-	{
-		Timeline notHoveredAnimation = new Timeline();
-		KeyFrame keyframe1 = new KeyFrame(javafx.util.Duration.ZERO, new KeyValue(coverPage.translateYProperty(),0));
-		KeyFrame keyframe2 = new KeyFrame(javafx.util.Duration.millis(50), new KeyValue(coverPage.translateYProperty(),5));
-		notHoveredAnimation.getKeyFrames().addAll(keyframe1,keyframe2);
-	}
-	public void MouseLeft(ImageView coverPage)
-	{
-        int index = centerBox.getChildren().indexOf(coverPage.getParent());
-        if (index >= 0 && index < notHoveredAnimations.size()) {
-            notHoveredAnimations.get(index).play();
-        }
-	}
-
 	public void handleResize() 
 	{
 		centerBox.setPrefWidth(658);
 		centerBox.setPrefHeight(780);
 	}
-
-	public void getScrollbarPosition() 
-	{
-		double value = scrollPane.getVvalue();
-
-		if (value == 1.0) {
-			centerBox.addRow(10);
-			System.out.println(centerBox.getPrefHeight());
-
-			System.out.println(centerBox.getRowCount());
-			System.out.println(centerBox.getColumnCount());
-		}
-	}
-
 }
