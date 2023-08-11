8c2a987b468b03968913c41afd2ad0aa38f81d11
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-11 23:29:59 +0530

    temporarily made the load window as the graph scene for testing purposes

--- bookFlowAnalytics/src/application/Main.java
+++ bookFlowAnalytics/src/application/Main.java
@@ -13,10 +13,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainWindow.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/bookflowAnalytics.fxml"));
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
-			mainWindowController MainWindow = loader.getController();
+//			mainWindowController MainWindow = loader.getController();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			Image icon = new Image("/logo.png"); 
 			primaryStage.getIcons().add(icon);
@@ -26,12 +26,12 @@ public class Main extends Application {
 			
 			primaryStage.setScene(scene);
 			
-			scene.setOnKeyPressed(Event ->{
-				if(Event.getCode() == KeyCode.ESCAPE)
-				{
-					MainWindow.handleResize();
-				}
-			});
+//			scene.setOnKeyPressed(Event ->{
+//				if(Event.getCode() == KeyCode.ESCAPE)
+//				{
+////					MainWindow.handleResize();
+//				}
+//			});
 			primaryStage.show();
 			
 		} catch(Exception e) {
