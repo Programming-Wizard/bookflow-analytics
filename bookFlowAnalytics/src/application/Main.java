858c3b96059b15c5dc405e7d51cb7c78a1ff92dc
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-12 18:40:02 +0530

    made the primary stage the size of the screen now

--- bookFlowAnalytics/src/application/Main.java
+++ bookFlowAnalytics/src/application/Main.java
@@ -2,10 +2,12 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -15,25 +17,26 @@ public class Main extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainWindow.fxml"));
 			Parent root = loader.load();
-			Scene scene = new Scene(root);
+			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+			Scene scene = new Scene(root, screenBounds.getWidth(),(screenBounds.getHeight() - 27));
 			mainWindowController MainWindow = loader.getController();
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			Image icon = new Image("/logo.png"); 
 			primaryStage.getIcons().add(icon);
 			primaryStage.setTitle("BookFlow");
 //			primaryStage.setFullScreen(true);
-			primaryStage.setMaximized(true);
 			primaryStage.setResizable(false);
-
+			
 			primaryStage.setScene(scene);
+
 
 			MainWindow.graphSceneButton.setOnMouseClicked(Event ->{
 				try {
 					 	FXMLLoader graphSceneLoad = new FXMLLoader(getClass().getResource("/bookflowAnalytics.fxml"));
 	                    Parent newRoot = graphSceneLoad.load();
-	                    Scene graphScene = new Scene(newRoot);
+	                    Scene graphScene = new Scene(newRoot, screenBounds.getWidth(),(screenBounds.getHeight() - 27));
 	                    primaryStage.setScene(graphScene);
-	                    primaryStage.setResizable(true);
+	                    primaryStage.setResizable(false);
 	                    bookFlowAnalyticsController GraphSceneController = graphSceneLoad.getController();
 	                    GraphSceneController.backToMainWindow.setOnMouseClicked(Event2 ->{
 	                    	try {
