36897a6112dbee04539181347d5b22b00190f57c
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-12 14:41:35 +0530

    made the graph which user can see and decide which book to read based upon there ratings but right now it only has dummy data would make it fetch data from api in coming time

--- bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
+++ bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
@@ -3,6 +3,7 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
@@ -10,6 +11,7 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class bookFlowAnalyticsController implements Initializable {
 
@@ -17,6 +19,7 @@ public class bookFlowAnalyticsController implements Initializable {
 	private VBox rootVBox;
 	@FXML
 	private BarChart<String, Number> readsGraph;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 			CategoryAxis xAxis = new CategoryAxis();
@@ -24,19 +27,30 @@ public class bookFlowAnalyticsController implements Initializable {
 			xAxis.setAnimated(false);
 			yAxis.setAnimated(false);	
 			readsGraph.setAnimated(true);
-			xAxis.setLabel("Label");
-			yAxis.setLabel("Reads");
+			xAxis.setLabel("Books");
+			yAxis.setLabel("Rating");
 			
 			XYChart.Series<String, Number> series = new XYChart.Series<>();
 			series.getData().add(new XYChart.Data<>("Book A", 1.5));
 			series.getData().add(new XYChart.Data<>("Book B", 5));
 			series.getData().add(new XYChart.Data<>("Book C", 4.2));
 			series.getData().add(new XYChart.Data<>("Book D", 3.6));
+			series.getData().add(new XYChart.Data<>("Book E", 6.6));
+			series.getData().add(new XYChart.Data<>("Book F", 7.6));
+			series.getData().add(new XYChart.Data<>("Book G", 3.9));
+			series.getData().add(new XYChart.Data<>("Book H", 5.4));
+			series.getData().add(new XYChart.Data<>("Book I", 7.5));
+			series.getData().add(new XYChart.Data<>("Book J", 7.9));
+			series.getData().add(new XYChart.Data<>("Book K", 6.4));
+			series.getData().add(new XYChart.Data<>("Book L", 4.8));
+			series.getData().add(new XYChart.Data<>("Book M", 6.6));
+			series.getData().add(new XYChart.Data<>("Book N", 8.6));
+			series.getData().add(new XYChart.Data<>("Book O", 7.7));
 			
 			readsGraph.setPrefHeight(900);
-			
+
+
 			readsGraph.getData().add(series);
-			
 	}
 	
 }
