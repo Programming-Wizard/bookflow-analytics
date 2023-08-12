f828311a1d41a352d12b9037792a66f23d1d9300
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-12 17:54:48 +0530

    now the graph displays data fetched from the api

--- bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
+++ bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -20,39 +21,46 @@ public class bookFlowAnalyticsController implements Initializable {
 	private BarChart<String, Number> readsGraph;
 	@FXML
 	public ImageView backToMainWindow;
-	
+
+	private GoogleBooksApiClient apiClient;
+
+	private String maxResultsPerPage = "20";
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-			CategoryAxis xAxis = new CategoryAxis();
-			NumberAxis yAxis = new NumberAxis(0,5,0.5);
-			xAxis.setAnimated(false);
-			yAxis.setAnimated(false);	
-			readsGraph.setAnimated(true);
-			xAxis.setLabel("Books");
-			yAxis.setLabel("Rating");
-			
-			XYChart.Series<String, Number> series = new XYChart.Series<>();
-			series.getData().add(new XYChart.Data<>("Book A", 1.5));
-			series.getData().add(new XYChart.Data<>("Book B", 5));
-			series.getData().add(new XYChart.Data<>("Book C", 4.2));
-			series.getData().add(new XYChart.Data<>("Book D", 3.6));
-			series.getData().add(new XYChart.Data<>("Book E", 6.6));
-			series.getData().add(new XYChart.Data<>("Book F", 7.6));
-			series.getData().add(new XYChart.Data<>("Book G", 3.9));
-			series.getData().add(new XYChart.Data<>("Book H", 5.4));
-			series.getData().add(new XYChart.Data<>("Book I", 7.5));
-			series.getData().add(new XYChart.Data<>("Book J", 7.9));
-			series.getData().add(new XYChart.Data<>("Book K", 6.4));
-			series.getData().add(new XYChart.Data<>("Book L", 4.8));
-			series.getData().add(new XYChart.Data<>("Book M", 6.6));
-			series.getData().add(new XYChart.Data<>("Book N", 8.6));
-			series.getData().add(new XYChart.Data<>("Book O", 7.7));
-			
-			readsGraph.setPrefHeight(900);
+
+		apiClient = new GoogleBooksApiClient();
+
+		CategoryAxis xAxis = new CategoryAxis();
+		NumberAxis yAxis = new NumberAxis(0,5,0.5);
+		xAxis.setAnimated(false);
+		yAxis.setAnimated(false);	
+		readsGraph.setAnimated(true);
+		xAxis.setLabel("Books");
+		yAxis.setLabel("Rating");
+
+		XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+		String query = "fiction";
+
+		List<Book> books = apiClient.fetchBooksData(query,maxResultsPerPage);
+
+		for (Book book : books) {
+			double rating = book.getRating();
+			if(rating > 0.0)
+			{
+				String bookName = book.getTitle();
+				series.getData().add(new XYChart.Data<>(bookName, rating));
+			}
+
+		}
 
 
-			readsGraph.getData().add(series);
+		readsGraph.setPrefHeight(900);
+
+
+		readsGraph.getData().add(series);
 	}
-	
-	
+
+
 }
