77ed52fb70d65d7ee423057d97d03f0e3c19fb65
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-13 17:39:01 +0530

    removed the title bar of the review and added a custom close button and also handled the click event on it

--- bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
+++ bookFlowAnalytics/src/application/bookFlowAnalyticsController.java
@@ -2,6 +2,7 @@ package application;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -25,46 +26,48 @@ public class bookFlowAnalyticsController implements Initializable {
 	private GoogleBooksApiClient apiClient;
 
 	private String maxResultsPerPage = "20";
-	
-	public static String query = "harry potter";
+	String genres[] = {"Action Adventure", "Mystery", "Thriller", "Horror", "Science-Fiction", "Fantasy",
+			"Self-Help", "Travel", "Cooking", "Science","Technology", "Business", "History",
+			"Art", "Music", "Poetry", "Graphic Novels", "Comics", "Children", "Classic", "Fairy-Tales",
+			"Folklore", "Mythology","Western", "Crime", "Medical", "Political",
+			"Spy", "War", "Western","Aliens", "Cyberpunk", "Post-Apocalyptic", "Space-Opera", "Steampunk",
+			"Urban-Fantasy"  };
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
+		Random random = new Random();
+		int randomIndex = random.nextInt(genres.length - 1);
+		String selectedGenre = genres[randomIndex];
+		String query = selectedGenre;
 		mainWindowController mainWindow = new mainWindowController();
 
 		apiClient = new GoogleBooksApiClient();
 
 		CategoryAxis xAxis = new CategoryAxis();
-		NumberAxis yAxis = new NumberAxis(0,5,0.5);
+		NumberAxis yAxis = new NumberAxis(0, 5, 0.5);
 		xAxis.setAnimated(false);
-		yAxis.setAnimated(false);	
+		yAxis.setAnimated(false);
 		readsGraph.setAnimated(true);
 		xAxis.setLabel("Books");
 		yAxis.setLabel("Rating");
 
 		XYChart.Series<String, Number> series = new XYChart.Series<>();
 
-		
-
-		List<Book> books = apiClient.fetchBooksData(query,maxResultsPerPage);
+		List<Book> books = apiClient.fetchBooksData(query, maxResultsPerPage);
 
 		for (Book book : books) {
 			double rating = book.getRating();
-			if(rating > 0.0)
-			{
+			if (rating > 0.0) {
 				String bookName = book.getTitle();
 				series.getData().add(new XYChart.Data<>(bookName, rating));
 			}
 
 		}
 
-
 		readsGraph.setPrefHeight(900);
-
 
 		readsGraph.getData().add(series);
 	}
-
 
 }
