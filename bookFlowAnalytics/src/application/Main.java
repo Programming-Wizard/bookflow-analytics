5cae49a4ddc5e4fdc058410fd5e37f2f6f52aa60
Programming-Wizard <varun.02.11.04@gmail.com>
2023-08-14 20:22:50 +0530

    added many changes to this class added a listener to home button and also defined a flag to know if the user is on the home screen or not

--- bookFlowAnalytics/src/application/Main.java
+++ bookFlowAnalytics/src/application/Main.java
@@ -6,42 +6,58 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	public static boolean isOnHomeScreen = true;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			Image icon = new Image("/logo.png"); 
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/loadingScreen.fxml"));
 			Parent root = loader.load();
 			Scene loadingScene = new Scene(root);
 			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 			Stage loadingStage = new Stage();
+			loadingStage.setTitle("Loading Books For You :");
+			loadingStage.setResizable(false);
+			loadingStage.getIcons().add(icon);
 			loadingStage.setScene(loadingScene);
 			loadingStage.show();
-			System.out.println("showed the loading screen");
 
 			loadMainWindowTask LoadMainWindowTask = new loadMainWindowTask();
-			System.out.println("loaded the task initiated as well");
-			System.out.println("checking if it succeded in it ");
 			LoadMainWindowTask.setOnSucceeded(event1 -> {
-				System.out.println("it succeded");
 				Parent mainRoot = LoadMainWindowTask.getValue();
 				Scene mainScene = new Scene(mainRoot);
 				primaryStage.setScene(mainScene);
-				System.out.println("stage is about to be showed");
+				primaryStage.getIcons().add(icon);
 				primaryStage.setTitle("Bookflow");
 				primaryStage.show();
 
 				mainWindowController MainWindow = LoadMainWindowTask.getController();
-				System.out.println("got the the controller");
 
 				loadingStage.close();
-				System.out.println("closed the loading screen");
+				MainWindow.getHomeButton().setOnMouseClicked(Event4 ->{
+					if(isOnHomeScreen == false) {
+						isOnHomeScreen = true;
+						GoogleBooksApiClient apiClient = new GoogleBooksApiClient();
+						String maxResultsPerPage = "20";
+						GridPane centerBox = MainWindow.getCenterBox();
+						ImageView coverPage = MainWindow.coverPage;
 
-				
+						centerBox.getChildren().clear();
+						GoToHomeTask goToHomeTask = new GoToHomeTask(apiClient, maxResultsPerPage, centerBox, MainWindow.getLoadingSpinner(), coverPage, MainWindow);
+
+						Thread th3 = new Thread(goToHomeTask);
+						th3.setDaemon(true);
+						th3.start();
+					}
+				});
+
 				MainWindow.graphSceneButton.setOnMouseClicked(Event2 ->{
 					try {
 						FXMLLoader graphSceneLoad = new FXMLLoader(getClass().getResource("/bookflowAnalytics.fxml"));
@@ -66,14 +82,12 @@ public class Main extends Application {
 					{
 						e.printStackTrace();
 					}
-					System.out.println("thread is about to start");
 				});
 
 			});
 			Thread th2 = new Thread(LoadMainWindowTask);
 			th2.setDaemon(true);
 			th2.start();
-			System.out.println("process ended");
 
 		} catch(Exception e) {
 			e.printStackTrace();
